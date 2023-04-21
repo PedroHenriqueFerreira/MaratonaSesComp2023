68fa8cfe146020f685e02987d09859a1c8266f6a
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2023-04-21 18:09:19 -0300

    Atv 09

--- atv9.py
+++ atv9.py
@@ -1,7 +1,7 @@
-res = 0
+res = []
 
 while True:
-    n, m = [int(v) for v in input('').splt(' ')]
+    n, m = [int(v) for v in input('').split(' ')]
     
     if n == 0 and m == 0:
         break
@@ -9,11 +9,34 @@ while True:
     matriz = []
     
     for i in range(n):
-        line = [int(v) for v in input('').split(' ')]
+        line = [bool(int(v)) for v in input('').split(' ')]
         matriz.append(line)
     
-    rule_1 = rule_2 = rule_3 = rule_4 = 0
+    rule_1 = True
+    rule_4 = True
     
-    for i in n:
-        for j in m:
-            if 
+    for i in range(n):
+        if matriz[i].count(True) == m:
+            rule_1 = False
+            
+        if matriz[i].count(True) == 0:
+            rule_4 = False
+    
+    rule_2 = True
+    rule_3 = True
+    
+    for j in range(m):
+        col = [matriz[i][j] for i in range(n)]
+        
+        if col.count(True) == 0:
+            rule_2 = False
+        
+        if col.count(True) == n:
+            rule_3 = False
+            
+    res.append([rule_1, rule_2, rule_3, rule_4].count(True))
+
+print('')
+    
+for value in res:
+    print(value)
